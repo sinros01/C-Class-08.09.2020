7782995704c7c7bf07a424744f9e4d208a92ac97
sinros01 <69473399+sinros01@users.noreply.github.com>
2020-09-08 14:33:55 +0200

    used switch cases instead of if sentences

--- C++ Class 08.09.2020/C++ Class 08.09.2020.cpp
+++ C++ Class 08.09.2020/C++ Class 08.09.2020.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+
 int main() {
 	char option;
 	char a = 'A';
@@ -12,7 +13,7 @@ int main() {
 		std::cout << "C. select difficulty" << std::endl;
 		std::cout << "Select an option:";
 		std::cin >> option;
-		if (option == a) {
+		/*if (option == a) {
 			std::cout << "Saving..." << std::endl;
 		}
 		else if (option == b) {
@@ -23,6 +24,22 @@ int main() {
 		}
 		else {
 			std::cout << "Invalid input" << std::endl;
+		} */
+
+		switch (option) {
+		case 'A': case 'a':
+			std::cout << "Saving..." << std::endl;
+			break;
+		case 'B': case 'b':
+			std::cout << "restarting the game..." << std::endl;
+			break;
+		case 'C': case 'c':
+			std::cout << "Please select difficulty (easy/hard)" << std::endl;
+			break;
+
+		default:
+			std::cout << "Invalid input" << std::endl;
+			break;
 		}
-	} while (true);
+	} while (true); 
 }
